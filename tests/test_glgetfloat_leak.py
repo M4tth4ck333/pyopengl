82dbeeaefb2bbaae286dee7c7ab8d7992b003431
Mike C. Fletcher <mcfletch@vrplumber.com>
2026-05-09 22:18:00 -0400

    TESTSUITE Bugfix for test case checking absolute equality

--- tests/test_glgetfloat_leak.py
+++ tests/test_glgetfloat_leak.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 from __future__ import print_function
-import sys, os
+import sys, os, math
 import pygame
 from pygame.locals import *
 from OpenGL.GL import *
@@ -14,21 +14,20 @@ def main():
     glClearColor(1.0, 0.0, 0.0, 1.0)
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
 
-    done = False
+    proc = psutil.Process(os.getpid())
+    mem = None
+    for i in range(0, 500):
+        if i == 10:
+            mem = proc.memory_percent()
+        if i > 400:
+            new_mem = proc.memory_percent()
+            # Allow small allocator/GC noise; a real leak grows orders of magnitude more.
+            assert math.isclose(new_mem, mem, rel_tol=1e-3), (new_mem, mem)
+            break
 
-    while not done:
-        mem = 0
-        for i in range(0, 500):
-            if i == 10:
-                mem = psutil.Process(os.getpid()).memory_percent()
-            if i > 400:
-                new_mem = psutil.Process(os.getpid()).memory_percent()
-                assert new_mem == mem, (new_mem, mem)
-                break
+        modelview_matrix = glGetFloatv(GL_MODELVIEW_MATRIX)
+        assert modelview_matrix is not None
 
-            modelview_matrix = glGetFloatv(GL_MODELVIEW_MATRIX)
-            assert modelview_matrix is not None
-        break
     sys.stdout.write('OK\n')
     sys.stdout.flush()
 
